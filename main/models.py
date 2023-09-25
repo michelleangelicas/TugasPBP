507132539dac0d7f44e15c627c16a7b3666f06ab
michelleangelicas <michelleangelica1907@gmail.com>
2023-09-25 16:36:14 +0700

    tugas4

--- main/models.py
+++ main/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
     amount = models.IntegerField()
     description = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
